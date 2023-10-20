44fa0dd4a80c1e4ff9fbcb1d032d9324cd07e3ff
Lucas froidefond <lucas.froidefond@mmibordeaux.com>
2023-10-20 15:47:00 +0200

    header fix

--- media-spe/src/app/(DashBoard)/Components/Header/Header.js
+++ media-spe/src/app/(DashBoard)/Components/Header/Header.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
-import React from "react";
+import style from "./header.module.scss";
 
 function Header() {
   return (
     <div className={style.header}>
-      <Link href="/admin/articles">Gestion des articles</Link>
+      <Link href="/admin/articles" className={style.button}>
+        Gestion des articles
+      </Link>
+      <Link href="/admin/articles" className={style.button}>
+        Creation d'un article
+      </Link>
+      <Link href="/admin/articles" className={style.button}>
+        Gestion des commentaires
+      </Link>
     </div>
   );
 }
