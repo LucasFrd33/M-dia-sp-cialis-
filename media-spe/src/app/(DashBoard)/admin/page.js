4ec363a5d9871948304f5871c3b8d8d883a6872d
Lucas froidefond <lucas.froidefond@mmibordeaux.com>
2023-10-23 10:26:22 +0200

    router refresh in CreateArticleForm

--- media-spe/src/app/(DashBoard)/admin/page.js
+++ media-spe/src/app/(DashBoard)/admin/page.js
@@ -1,8 +1,12 @@
 import style from "./Style/HomeAdmin.module.scss";
-import CreateArticlesForm from "../Components/CreateArticlesForm/CreateArticlesForm";
+import CreateArticlesForm from "./articles-create/Components/CreateArticlesForm/CreateArticlesForm";
 
 function Admin() {
-  return <div className={`${style.home}`}><CreateArticlesForm /></div>;
+  return (
+    <div className={`${style.home}`}>
+      <CreateArticlesForm />
+    </div>
+  );
 }
 
 export default Admin;
