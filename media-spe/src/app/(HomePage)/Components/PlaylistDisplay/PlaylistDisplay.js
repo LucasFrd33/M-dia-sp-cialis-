38e4f5f53fea1981a3c699d7bdd38d46936d21a8
ODrimm <ystos.gaming@gmail.com>
2023-10-27 12:31:40 +0200

    comment fix vercel?

--- media-spe/src/app/(HomePage)/Components/PlaylistDisplay/PlaylistDisplay.js
+++ media-spe/src/app/(HomePage)/Components/PlaylistDisplay/PlaylistDisplay.js
@@ -41,7 +41,7 @@ function PlaylistDisplay({ lastArt }) {
       </section>
 
 
-      <section className={style.main}>
+      <section className={style.main} onClick={() => setIsPlaying(true)}>
         <section className={style.description}>
           <section className={style.image}>
             <img className={style.miniature} src={`data:image/jpeg;base64,${artMiniature}`} />
@@ -49,7 +49,7 @@ function PlaylistDisplay({ lastArt }) {
               <path d="M28.4849 0C22.8511 0 17.3438 1.67061 12.6595 4.80056C7.97522 7.93052 4.32424 12.3792 2.16829 17.5842C0.012342 22.7891 -0.551753 28.5164 0.547341 34.042C1.64644 39.5675 4.35935 44.643 8.34303 48.6267C12.3267 52.6104 17.4022 55.3233 22.9277 56.4224C28.4533 57.5215 34.1806 56.9574 39.3855 54.8014C44.5905 52.6455 49.0392 48.9945 52.1691 44.3102C55.2991 39.6259 56.9697 34.1186 56.9697 28.4848C56.9697 24.7442 56.2329 21.0401 54.8014 17.5842C53.3699 14.1282 51.2717 10.9881 48.6267 8.34302C45.9816 5.69796 42.8415 3.59978 39.3855 2.16828C35.9296 0.736783 32.2255 0 28.4849 0ZM22.7879 41.303V15.6667L39.8788 28.4848L22.7879 41.303Z" fill="#FAFAFA" />
             </svg>
           </section>
-          <button className={style.button} onClick={() => setIsPlaying(true)}>{(artType == "podcast" && "Voir la playlist" || artType == "short" && "Voir les vidéos" || artType == "video" && "Voir les vidéos" || "articles")}</button>
+          <button className={style.button} >{(artType == "podcast" && "Voir la playlist" || artType == "short" && "Voir les vidéos" || artType == "video" && "Voir les vidéos" || "articles")}</button>
         </section>
 
         <h2>{lastArt.title}</h2>
